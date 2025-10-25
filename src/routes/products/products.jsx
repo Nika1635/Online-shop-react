d9784f7289e0efb23342345acc5e62ca5ebcc93f
nika robakidze <nika70252@gmail.com>
2025-10-26 01:44:40 +0400

    banner

--- src/routes/products/products.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom"
-import "./products.css"
-
-export default function Products(){
-    return<>
-        <h1></h1>
-        <Link to="/">Go to index</Link>
-    </>
-} 
