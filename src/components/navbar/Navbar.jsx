a79f0c0ff3c12059b9e73fa3a58389f900be389a
nika robakidze <nika70252@gmail.com>
2025-10-27 00:52:51 +0400

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,12 +4,17 @@ import { Link } from "react-router-dom"
 export default function Navbar(){
     return <nav>
         <ul className="navbar-list">
-            <li><Link to="/" className="navbar-list-item">home</Link></li>
-            <li><Link to="/products" className="navbar-list-item">products</Link></li>
+            <li><Link to="/" className="navbar-list-item">Home</Link></li>
+            <li><Link to="/products" className="navbar-list-item">Products</Link></li>
         </ul>
 
-        <div>
+        <div className="search-hero">
+            <input type="text"/><i class="fa-solid fa-magnifying-glass"></i>
+        </div>
 
+        <div className="icon-list">
+            <i class="fa-solid fa-right-to-bracket"></i>
+            <i class="fa-solid fa-cart-shopping"></i>
         </div>
         
     </nav>
